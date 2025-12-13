46984c27fb7e4389b73172449230b2509af29a3c
yanovychmark <143933956+yanovychmark@users.noreply.github.com>
2025-12-13 10:26:01 +0100

    v1.3
    fixed some bugs and updated debug to have a prompt system

--- calculator.py
+++ calculator.py
@@ -1,39 +1,39 @@
 #made by markus sorry but i will not tell you what all this stuff does
-print("the amazing calculator v1.1 insert number of how many numbers use (1, 2, 3, 4) and then input what will the numbers be and select action - + / * "
+print("the amazing calculator v1.3 insert number of how many numbers use (1, 2, 3, 4) and then input what will the numbers be and select action - + / * "
       " for 4 -+ will be 1+1-1+1 and /* will be 1*1/1*1 */, +- will reverse placement")
 A = int(input("how many numbers? ---->"))
 if A == 1:
     b = int(input('number ---->'))
     print(b)
-if A == 2:
+elif A == 2:
     b = int(input('number ---->'))
     c = int(input('number ---->'))
     omega = input("action ------>")
     if omega == "-":
         print(b - c)
-    if omega == "+":
+    elif omega == "+":
         print(b + c)
-    if omega == "/":
+    elif omega == "/":
         if b == 0:
             print("04 check debug")
         elif c == 0:
             print("04 check debug")
         else:
             print(b / c)
-    if omega == "*":
+    elif omega == "*":
         print(b * c)
     else:
         print("if you got nothing check debug 01")
-if A == 3:
+elif A == 3:
     b = int(input('number ---->'))
     c = int(input('number ---->'))
     d = int(input('number ---->'))
     omega = input("action ------>")
     if omega == "-":
         print(b - c - d)
-    if omega == "+":
+    elif omega == "+":
         print(b + c + d)
-    if omega == "/":
+    elif omega == "/":
         if b == 0:
             print("04 check debug")
         elif c == 0:
@@ -42,11 +42,11 @@ if A == 3:
             print("04 check debug")
         else:
             print(b / c / d)
-    if omega == "*":
+    elif omega == "*":
         print(b * c * d)
     else:
         print("if you got nothing check debug 01")
-if A == 4:
+elif A == 4:
     b = int(input('number ---->'))
     c = int(input('number ---->'))
     d = int(input('number ---->'))
@@ -54,9 +54,9 @@ if A == 4:
     omega = input("action ------>")
     if omega == "-":
         print(b - c - d - e)
-    if omega == "+":
+    elif omega == "+":
         print(b + c + d + e)
-    if omega == "/":
+    elif omega == "/":
         if b == 0:
             print("04 check debug")
         elif c == 0:
@@ -67,13 +67,13 @@ if A == 4:
             print("04 check debug")
         else:
             print(b / c / d / e)
-    if omega == "*":
+    elif omega == "*":
         print(b * c * d * e)
-    if omega == "-+":
+    elif omega == "-+":
         print(b + c - d + e)
-    if omega == "+-":
+    elif omega == "+-":
         print(b - c + d - e)
-    if omega == "/*":
+    elif omega == "/*":
         if b == 0:
             print("04 check debug")
         elif c == 0:
@@ -84,7 +84,7 @@ if A == 4:
             print("04 check debug")
         else:
             print(b * c / d * e)
-    if omega == "*/":
+    elif omega == "*/":
         if b == 0:
             print("04 check debug")
         elif c == 0:
@@ -96,14 +96,22 @@ if A == 4:
         else:
             print(b / c * d / e)
     else:
-        print("if you got nothing check debug 01")
+        print("check debug 01")
 else:
-    print("if you got nothing check debug 02")
+    print("check debug 02")
 alpha = input("debug mode Y/N?")
-if alpha == "Y":
+if alpha == "Y" or "y":
     print("ValueError: invalid literal for int() with base 10: means that text was used for numbers only")
     print("01 no answear means that the action is wrong or is not a real one")
     print("02 no answear means that the number of numbers is to large or not correct")
     print("04 do not do that mistake (0/number)")
+    print("debug function is not correct")
     print("if you want to to calculate again restart the program")
+    delta = input("insert debug prompt---->")
+    if delta == "quit":
+        print("thanks for using")
+    elif delta == 'null': #insert cool debug features here
+        print("test")
+    else:
+        print("05 check debug")
 
